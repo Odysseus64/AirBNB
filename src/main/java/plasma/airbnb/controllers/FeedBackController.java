4e2827189260d224ac2b426bc54486ecc939afa0
Odysseys32 <xeon101110@gmail.com>
2023-05-30 17:17:09 +0600

    Update-0.0.3 Работаем только через swagger html и части фронтенда отменяем

--- src/main/java/plasma/airbnb/controllers/FeedBackController.java
+++ src/main/java/plasma/airbnb/controllers/FeedBackController.java
@@ -2,7 +2,10 @@ package plasma.airbnb.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import plasma.airbnb.reposiroty.FeedBackRepository;
 
+@Controller
+@RequiredArgsConstructor
 public class FeedBackController {
-
+    private final FeedBackRepository repository;
 }
--- src/main/java/plasma/airbnb/controllers/ImageController.java
+++ src/main/java/plasma/airbnb/controllers/ImageController.java
@@ -1,4 +1,11 @@
 package plasma.airbnb.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import plasma.airbnb.reposiroty.ImageRepository;
+
+@Controller
+@RequiredArgsConstructor
 public class ImageController {
+    private final ImageRepository repository;
 }
