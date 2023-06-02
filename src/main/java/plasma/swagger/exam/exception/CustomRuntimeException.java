06b6237517abb3b7af68d7b02b79d40275279c4b
kurmanbekdev <kurmanbekpazylo@gmail.com>
2023-06-02 16:16:30 +0600

    kman - new branch test -1

--- src/main/java/plasma/swagger/exam/exception/CustomRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package plasma.swagger.exam.exception;
-
-public class CustomRuntimeException extends RuntimeException {
-
-    public CustomRuntimeException(String message) {
-        super(message);
-    }
-}
