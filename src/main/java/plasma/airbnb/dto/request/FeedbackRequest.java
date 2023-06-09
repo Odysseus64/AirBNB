8ac609efb80417d2341e89ff493fdc519d4ab041
bakytapsamtov <apsamtovbakyt@gmail.com>
2023-06-09 15:04:36 +0600

    feat: add FeedBack #1

---
+++ src/main/java/plasma/airbnb/dto/request/FeedbackRequest.java
@@ -0,0 +1,8 @@
+package plasma.airbnb.dto.request;
+
+import lombok.Data;
+
+@Data
+public class FeedbackRequest {
+    private String feedback;
+}
---
+++ src/main/java/plasma/airbnb/dto/response/FeedbackResponse.java
@@ -0,0 +1,9 @@
+package plasma.airbnb.dto.response;
+
+import lombok.Data;
+
+@Data
+public class FeedbackResponse {
+    private Long feedbackId;
+    private String feedback;
+}
