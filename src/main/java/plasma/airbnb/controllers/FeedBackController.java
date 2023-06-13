59232a5f5c62d0d81513ca798765a074428d9146
bakytapsamtov <apsamtovbakyt@gmail.com>
2023-06-13 15:32:30 +0600

    feat: add FeedBack #1

--- src/main/java/plasma/airbnb/controllers/FeedBackController.java
+++ src/main/java/plasma/airbnb/controllers/FeedBackController.java
@@ -1,11 +1,42 @@
 package plasma.airbnb.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import plasma.airbnb.reposiroty.FeedBackRepository;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import plasma.airbnb.dto.request.FeedbackRequest;
+import plasma.airbnb.dto.response.FeedbackResponse;
+import plasma.airbnb.service.FeedBackService;
+
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/feedbacks")
 public class FeedBackController {
-    private final FeedBackRepository repository;
+
+    private final FeedBackService feedBackService;
+
+    @PostMapping("/saveFeedback")
+    public String saveFeedBack(@RequestBody FeedbackRequest feedbackRequest) {
+        feedBackService.saveFeedback(feedbackRequest);
+        return "Comments successfully save";
+    }
+    @DeleteMapping("deleteFeedback")
+    public String deleteFeedBack(@PathVariable Long id){
+        feedBackService.deleteById(id);
+        return "Comments successfully delete";
+    }
+    @PutMapping("/updateFeedback/{id}")
+    public String updateFeedBack(@PathVariable Long id,@RequestBody FeedbackRequest feedbackRequest){
+        feedBackService.update(id,feedbackRequest);
+        return "Comments successfully update";
+    }
+    @GetMapping("/getFeedback{id}")
+    public FeedbackResponse findById(@PathVariable Long id){
+        return feedBackService.findById(id);
+    }
+    @GetMapping("/getAllFeedback")
+    public List<FeedbackResponse> getAll(){
+        return feedBackService.findAll();
+    }
 }
