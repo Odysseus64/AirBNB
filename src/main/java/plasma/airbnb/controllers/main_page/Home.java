438491cd20443d960066a1dfc4509808f7e3f4d8
Odysseus64 <jotarokudjo01@gmail.com>
2023-06-14 22:54:08 +0600

    Исправили ошибки

--- src/main/java/plasma/airbnb/controllers/main_page/Home.java
+++ src/main/java/plasma/airbnb/controllers/main_page/Home.java
@@ -1,10 +1,14 @@
 package plasma.airbnb.controllers.main_page;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import plasma.airbnb.model.Product;
 import plasma.airbnb.service.ProductService;
 
 @Controller
@@ -13,17 +17,20 @@ import plasma.airbnb.service.ProductService;
 public class Home {
     private final ProductService service;
     @GetMapping("/home/")
-    public String houme(){
+    public ResponseEntity<Product> houme(){
         service.findAll();
-        return "Get All product";
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @GetMapping("/home/getById{id}")
-    public String findById(@RequestParam("id") Long id){
+    public ResponseEntity<Product> findById(@PathVariable("id") Long id){
         service.findById(id);
-        return "Id found " + id;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     @GetMapping("/home/sortByName")
-    public String sortByName(){
-        return "";
+    public ResponseEntity<Product> sortByName(String name, Product product){
+        // find by title Bektur sir Это ваша работа
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+    // Не удалось затестить проект, отсуствует сваггер
+    // в ветке kokodae есть конфлект нужно исправить как моэно быстрее
 }
