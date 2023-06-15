1068023a41eb495fde439bb2f4c45f2309be5f81
Bektur <Bekturhanazarov98@gmail.com>
2023-06-15 14:43:38 +0600

    Added sort

--- src/main/java/plasma/airbnb/reposiroty/ProductRepository.java
+++ src/main/java/plasma/airbnb/reposiroty/ProductRepository.java
@@ -1,6 +1,8 @@
 package plasma.airbnb.reposiroty;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import plasma.airbnb.enums.Region;
 import plasma.airbnb.model.Product;
@@ -9,5 +11,27 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Region> sortRole(Region region);
+    @Query("select s from Product s where s.title = :title ")
+    List<Product> findByTitle(@Param("title") String title);
+
+    @Query("select s from Product s where s.price = :price")
+    List<Product> findByPrice(@Param("price") String price);
+
+    @Query("select s from Product s where s.region = :region")
+    List<Product> findByRegion(@Param("region") String region);
+
+    @Query("select s from Product s where s.type = :type")
+    List<Product> findByType(@Param("type") String type);
+
+    @Query("select s from Product s order by s.title desc ")
+    List<Product> ratingTitle();
+
+    @Query("select s from Product s order by s.price desc ")
+    List<Product> ratingPrice();
+
+    @Query("select s from Product s order by s.region desc ")
+    List<Product> ratingRegion();
+
+    @Query("select s from Product s order by s.type desc ")
+    List<Product> ratingType();
 }
