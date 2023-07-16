a35fd3b41b29ad26c02d54bffafb693e7ecef791
Odysseus64 <jotarokudjo01@gmail.com>
2023-07-16 10:41:41 +0600

    Почему он все еще говорит 400 bad request???

--- src/main/java/plasma/airbnb/model/Product.java
+++ src/main/java/plasma/airbnb/model/Product.java
@@ -47,9 +47,12 @@ public class Product {
     private int rating;
     private LocalDateTime date_now = LocalDateTime.now();
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    @OneToMany
     private List<Image> images = new ArrayList<>();
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    @OneToMany
     private List<FeedBack> feedBacks = new ArrayList<>();
+    //TEST
 }
